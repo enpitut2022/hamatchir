a93f08662fa74fb73dc54d5b8664134e2a9a8184
ChinoShun <s2011569@s.tsukuba.ac.jp>
2022-10-21 18:09:27 +0900

    htmlページ表示(画像は×)

--- mysite/hamatchir/views.py
+++ mysite/hamatchir/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from matplotlib import image
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-    
+    myapp_data = {
+    'app': 'Django'
+    }
+    return render(request, 'index.html',myapp_data)
+
 # Create your views here.
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -117,9 +117,35 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+          os.path.join(BASE_DIR, 'templates'),
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+# staticフォルダのURLパスを設定
+STATIC_URL = '/static/'
+
+# staticフォルダの場所を設定
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
+
+# # デプロイ用に静的ファイルを収集するディレクトリを指定（開発環境では不要）
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
