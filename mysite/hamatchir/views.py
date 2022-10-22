614ba4e4e188c173153250844a5b00c7eaf59f5d
nabeman <tkmcoins20@gmail.com>
2022-10-22 13:00:13 +0900

    HttpResponseを使ったやり方を一応追加

--- mysite/hamatchir/views.py
+++ mysite/hamatchir/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    return render(request, 'hamatchir/index.html')
+    template = loader.get_template('hamatchir/index.html')
+    # return render(request, 'hamatchir/index.html')
+    return HttpResponse(template.render(None, request))
 
 # Create your views here.
