73c5619312f3064dffab295ad00811e534a60677
TKmatsu2008 <fhenixeaglu1st@icloud.com>
2022-11-02 10:27:03 +0900

    deployVercel

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +26,14 @@ SECRET_KEY = 'django-insecure-au&p0@j!2k5#2(lz$m4vhae#4vw)0b!_)io5&*()$4ey(e+uas
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    #vercel追加分
+        '*',
+    '.vercel.app'
+]
+
+#静的ファイルのビルドのためにSTATIC_ROOTを設定
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Application definition
@@ -37,7 +45,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hamatchir.apps.HamatchirConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,10 +82,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    #デプロイする上で一時的にコメントアウトしました。(Vercel)
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
@@ -118,12 +126,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# staticフォルダのURLパスを設定
-STATIC_URL = '/static/'
